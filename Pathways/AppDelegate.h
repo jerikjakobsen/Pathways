423b1995d16d327b29b98cfcbb1167fdc9d20723
John Jakobsen <jerikjakobsen@gmail.com>
2021-07-12 12:32:20 -0400

    Initial Commit

---
+++ Pathways/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Pathways
+//
+//  Created by johnjakobsen on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Pathways/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Pathways
+//
+//  Created by johnjakobsen on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
