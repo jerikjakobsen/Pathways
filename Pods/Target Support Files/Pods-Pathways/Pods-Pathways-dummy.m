1f1c808401be138b3665601c220f5b57ef9dc304
John Jakobsen <jerikjakobsen@gmail.com>
2021-07-22 17:17:17 -0400

    removing files again

--- Pods/Target Support Files/Pods-Pathways/Pods-Pathways-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Pathways : NSObject
-@end
-@implementation PodsDummy_Pods_Pathways
-@end
--- Pods/Target Support Files/Pods-PathwaysTests/Pods-PathwaysTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PathwaysTests : NSObject
-@end
-@implementation PodsDummy_Pods_PathwaysTests
-@end
